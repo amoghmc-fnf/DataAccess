03e6e6d659c34be40a936509de7bc82ff9107539
M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-07-17 13:15:07 +0530

    init ci

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,71 @@
+﻿
+using DataAccessRecap.Data;
+using DataAccessRecap.Models;
+using DataAccessRecap.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static IDataService service;
+        private static ServiceProvider CreateServices()
+        {
+            var services = new ServiceCollection()
+                .AddDbContext<StudentDbContext>()
+                .AddSingleton<IDataService, StudentDataService>()
+                .BuildServiceProvider();
+            return services;
+        }
+        static void Main(string[] args)
+        {
+            var services = CreateServices();
+            service = services.GetRequiredService<IDataService>();
+
+            //testUpdatingFeature();
+            testDeletingFeature();
+            testReadingFeature();
+            //testAddingFeature();
+        }
+
+        private static void testDeletingFeature()
+        {
+            service.DeleteStudent(1);
+        }
+
+        private static void testUpdatingFeature()
+        {
+            var student = new Student
+            {
+                ContactNo = 9889933333,
+                StudentName = "Phani Raj B N",
+                StudentEmail = "phanibn@gmail.com",
+                StudentId = 1
+            };
+            service.UpdateStudent(student);
+
+        }
+
+        private static void testReadingFeature()
+        {
+            var records = service.GetAllStudents();
+            foreach (var record in records)
+            {
+                Console.WriteLine(record);
+            }
+        }
+
+        private static void testAddingFeature()
+        {
+
+            var rec = new Student
+            {
+                StudentEmail = "Amogh",
+                StudentName = "amc@gmail.com",
+                ContactNo = 8923928425
+            };
+
+            service.AddNewStudent(rec);
+        }
+    }
+}
