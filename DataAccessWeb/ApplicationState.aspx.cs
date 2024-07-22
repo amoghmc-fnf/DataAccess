13b109f02101c5a15fa3fb58f32ad8af8e8d0a64
M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-07-22 15:33:22 +0530

    add application state

---
+++ DataAccessWeb/ApplicationState.aspx.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataAccessWeb
+{
+    public partial class ApplicationState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                if (Application["Items"] == null)
+                    Application.Add("Items", new List<string>());
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (Application["Items"] != null)
+            {
+                var data = Application["Items"] as List<string>;
+                data.Add(txtItem.Text);
+                Application["Items"] = data;
+                lstItems.DataSource = data;
+                lstItems.DataBind();
+            }
+        }
+    }
+}
---
+++ DataAccessWeb/SessionState.aspx.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DataAccessWeb
+{
+    public partial class SessionState : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+                if (Session["Items"] == null)
+                    Session.Add("Items", new List<string>());
+        }
+
+        protected void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (Session["Items"] == null)
+            {
+                lblError.Text = "No session info of the name Items is stored!";
+                return;
+            }
+            var oldItems = Session["Items"] as List<string>;
+            oldItems.Add(txtItem.Text);
+            Session["Items"] = oldItems;
+            lblError.Text = "Item added to the session";
+
+            lstItems.DataSource = oldItems;
+            lstItems.DataBind();
+        }
+    }
+}
