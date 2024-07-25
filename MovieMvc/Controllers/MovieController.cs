e36a18ff6a5493f5e159a50df8a0e005743c8797
M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-07-25 12:30:16 +0530

    add movie mvc

---
+++ MovieMvc/Controllers/MovieController.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieMvc.Models;
+
+namespace MovieMvc.Controllers
+{
+    public class MovieController : Controller
+    {
+        private readonly MovieDbContext _movieDbContext;
+
+        public MovieController(MovieDbContext movieDbContext)
+        {
+            _movieDbContext = movieDbContext;
+        }
+
+        public IActionResult AllMovies()
+        {
+            var movies = _movieDbContext.Movies.ToList();
+            movies.Sort();
+            return View(movies);
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null)
+                return NotFound();
+
+            var record = _movieDbContext.Movies.FirstOrDefault(m => m.Id == id);
+
+            if (record == null)
+                return NotFound();
+            return View(record);
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Movie postedData)
+        {
+            var id = postedData.Id;
+            var record = _movieDbContext.Movies.FirstOrDefault(m => m.Id == id);
+            if (record == null)
+                return NotFound();
+
+            record.Title = postedData.Title;
+            record.Description = postedData.Description;
+            record.Director = postedData.Director;
+            record.Cast = postedData.Cast;
+            record.Rating = postedData.Rating;
+            _movieDbContext.SaveChanges();
+            return RedirectToAction("AllMovies");
+        }
+
+        public IActionResult Create() => View(new Movie());
+
+        [HttpPost]
+        public IActionResult Create(Movie movie)
+        {
+            if (movie != null)
+            {
+                _movieDbContext.Movies.Add(movie);
+                _movieDbContext.SaveChanges();
+                return RedirectToAction("AllMovies");
+            }
+            return NotFound();
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+            var record = _movieDbContext.Movies.FirstOrDefault(m => m.Id == id);
+            if (record == null)
+                return NotFound();
+            return View(record);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Movie postedData)
+        {
+            var id = postedData.Id;
+            var record = _movieDbContext.Movies.FirstOrDefault(m => m.Id == id);
+            if (record == null)
+                return NotFound();
+
+            _movieDbContext.Remove(record);
+            _movieDbContext.SaveChanges();
+            return RedirectToAction("AllMovies");
+        }
+    }
+}
---
+++ MovieMvc/Models/Movie.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MovieMvc.Models
+{
+    [Table("Movie")]
+    public class Movie : IComparable<Movie>
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Director { get; set; }
+        public string Cast {  get; set; }
+        public int Rating { get; set; }
+
+        public int CompareTo(Movie? other)
+        {
+            var ratingComparison = this.Rating.CompareTo(other.Rating);
+            if (ratingComparison == 0)
+                return this.Title.CompareTo(other.Title);
+            return -ratingComparison;
+        }
+    }
+
+    public class MovieDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public MovieDbContext(DbContextOptions options) : base(options) { }
+    }
+}
