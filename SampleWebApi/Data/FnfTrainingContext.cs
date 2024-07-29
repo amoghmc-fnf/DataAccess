0f06bce5783e5ec34890ef7fcdcee6838006ab09
M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-07-29 16:10:55 +0530

    add sampleWebApi

---
+++ SampleWebApi/Data/FnfTrainingContext.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace SampleWebApi.Data;
+
+public partial class FnfTrainingContext : DbContext
+{
+    public FnfTrainingContext()
+    {
+    }
+
+    public FnfTrainingContext(DbContextOptions<FnfTrainingContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<StockTable> StockTables { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=FNFIDVPRE20529\\SQLSERVER; Database=FnfTraining; Trusted_Connection=True; TrustServerCertificate=True");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+
+        modelBuilder.Entity<StockTable>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("PK__StockTab__3214EC07499B6461");
+
+            entity.ToTable("StockTable");
+
+            entity.Property(e => e.Description).IsUnicode(false);
+            entity.Property(e => e.Name)
+                .HasMaxLength(100)
+                .IsUnicode(false);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ SampleWebApi/Data/StockTable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SampleWebApi.Data;
+
+public partial class StockTable
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public string Description { get; set; } = null!;
+
+    public double UnitPrice { get; set; }
+}
