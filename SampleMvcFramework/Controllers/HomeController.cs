941678f9f545b54d9cc78263df86a7554cb7c075
M Cheluvaraj <Amogh.MCheluvaraj@fnf.com>
2024-07-29 10:01:19 +0530

    add asp mvc framework

---
+++ SampleMvcFramework/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using SampleMvcFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SampleMvcFramework.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello world!";
+        }
+
+        public ViewResult Display()
+        {
+            var model = new Stock { Id = 123, Name = "HDFC Equity Fund", Description = "Stock investments on infrastructure", UnitPrice = 13.42235 };
+            return View(model);
+        }
+    }
+}
---
+++ SampleMvcFramework/Models/Stock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SampleMvcFramework.Models
+{
+    public class Stock
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double UnitPrice { get; set; }
+
+        //public override string ToString()
+        //{
+        //    return $"<h1>Stock Name: {StockName}</h1><hr/>";
+        //}
+    }
+}
